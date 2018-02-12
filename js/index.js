ef4a8bfab26892c8936992d7b7e72dba108d958f
Jason Long <jlong@github.com>
2018-02-12 16:57:52 -0500

    Toggle site nav

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ searchLink.addEventListener('click', function(e) {
   e.target.closest('.js-site-header').classList.toggle('is-searching');
   searchInput.focus();
 }, false);
+
+const navLink = document.querySelector('.js-nav-link');
+
+navLink.addEventListener('click', function(e) {
+  const siteNav = document.querySelector('.js-site-nav');
+  siteNav.classList.toggle('dn');
+}, false);
