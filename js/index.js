e7da517176ec6d8d6dd420684159c8f0d623d019
Jason Long <jlong@github.com>
2018-02-12 11:34:52 -0500

    Toggle search on mobile

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// alert('foo')
+const searchLink = document.querySelector('.js-search-link');
+
+searchLink.addEventListener('click', function(e) {
+  const searchInput = document.querySelector('.js-search-input');
+
+  e.target.closest('.js-site-header').classList.toggle('is-searching');
+  searchInput.focus();
+}, false);
